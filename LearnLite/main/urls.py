98a2e9604d3ae450191b12e40817a94fc235600f
Salemhu <salem.hussain.alsalem@gmail.com>
2024-04-22 15:02:32 +0300

    edited navbar.

--- LearnLite/main/urls.py
+++ LearnLite/main/urls.py
@@ -5,4 +5,6 @@ app_name = "main"
 
 urlpatterns  = [
     path("", views.index_view, name="index_view"),
+    path("contact/", views.contact_view, name="contact_view"),
+    path("about/", views.about_view, name="about_view"),
 ]
--- LearnLite/main/views.py
+++ LearnLite/main/views.py
@@ -6,3 +6,11 @@ from django.http import HttpRequest, HttpResponse
 def index_view(request:HttpRequest):
 
     return render(request, "main/index.html")
+
+def contact_view(request:HttpRequest):
+
+    return render(request, "main/contact.html")
+
+def about_view(request:HttpRequest):
+
+    return render(request, "main/about.html")
