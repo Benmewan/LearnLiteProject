5ebe1eb60806f9be1e846816abb569d68280d93d
Hammam <hajeddawi@gmail.com>
2024-04-22 13:43:35 +0300

    update settings

--- LearnLite/tasks/models.py
+++ LearnLite/tasks/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Task(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    due_date = models.DateTimeField(auto_now_add=False)
+    is_done = models.BooleanField()
