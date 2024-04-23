6ea5af151ad0dacbc8edcab5491018f974eeed49
Abdullah <.alhumidi.Ab@gmail.com>
2024-04-24 01:26:16 +0300

    the ability to generate  summary is wokring

--- LearnLite/LearnLite/urls.py
+++ LearnLite/LearnLite/urls.py
@@ -23,5 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("main.urls")),
     path('tasks/', include("tasks.urls")),
-    path('accounts/', include("accounts.urls"))
+    path('accounts/', include("accounts.urls")),
+    path('summary/', include("summary.urls")),
+
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
