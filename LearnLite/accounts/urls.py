fc501d901fe6162e317b6499265af255e853b112
Abdulaziz <benmrwan@hotmail.com>
2024-04-25 14:48:05 +0300

    Updated and adding profile page and settings page

--- LearnLite/accounts/urls.py
+++ LearnLite/accounts/urls.py
@@ -6,5 +6,7 @@ app_name = "accounts"
 urlpatterns  = [
     path('register/', views.register_user, name='register_user'),
     path('login/', views.user_login, name='user_login'),
-    path('logout/', views.user_logout, name='user_logout')
+    path('logout/', views.user_logout, name='user_logout'),
+    path('profile/', views.profile, name='profile'),
+    path('profile/settings/', views.profile_settings, name='profile_settings'),
 ]
