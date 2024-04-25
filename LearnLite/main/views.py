1cfbb8985dc5e1e644a803ffce80710cb26a40a5
Salemhu <salem.hussain.alsalem@gmail.com>
2024-04-25 11:17:48 +0300

    added contact model and user message page.

--- LearnLite/main/views.py
+++ LearnLite/main/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpRequest, HttpResponse
-
+from datetime import date, timedelta
+from .models import Contact
 # Create your views here.
 
 def index_view(request:HttpRequest):
@@ -8,9 +9,23 @@ def index_view(request:HttpRequest):
     return render(request, "main/index.html")
 
 def contact_view(request:HttpRequest):
-
+    if request.method == "POST":
+            new_message = Contact(name=request.POST["name"], 
+                                  email=request.POST["email"],  
+                                  message= request.POST["message"],)
+            
+            new_message.save()
     return render(request, "main/contact.html")
 
 def about_view(request:HttpRequest):
 
     return render(request, "main/about.html")
+
+def message_view(request:HttpRequest):
+    if not request.user.is_superuser:
+        # Render index template for non-staff users
+        return render(request, "main/index.html")
+
+    messages = Contact.objects.all()
+    
+    return render(request, "main/message.html", {"messages" : messages})
