c21e39208bce263bb5a294e90cc4e1cee6839545
Salemhu <salem.hussain.alsalem@gmail.com>
2024-04-25 14:28:40 +0300

    added dark and light mode views.

--- LearnLite/main/urls.py
+++ LearnLite/main/urls.py
@@ -8,4 +8,6 @@ urlpatterns  = [
     path("contact/", views.contact_view, name="contact_view"),
     path("about/", views.about_view, name="about_view"),
     path("message/", views.message_view, name="message_view"),
+    path("mode/dark/", views.dark_mode_view, name="dark_mode_view"),
+    path("mode/light/", views.light_mode_view, name="light_mode_view"),
 ]
